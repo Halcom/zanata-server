f6b399eee2d7a800c0ab7363c6de894a8f95be9c
Patrick Huang <pahuang@redhat.com>
2015-08-19 17:11:40 +1000

    replace s:decorate with zanata:decorate

--- functional-test/src/main/java/org/zanata/page/account/InactiveAccountPage.java
+++ functional-test/src/main/java/org/zanata/page/account/InactiveAccountPage.java
@@ -43,13 +43,13 @@ public class InactiveAccountPage extends BasePage {
     }
 
     public InactiveAccountPage enterNewEmail(String email) {
-        readyElement(By.id("inactiveAccountForm:emailField:email"))
+        readyElement(By.id("inactiveAccountForm:email:input:email"))
                 .sendKeys(email);
         return new InactiveAccountPage(getDriver());
     }
 
     public HomePage clickUpdateEmail() {
-        clickElement(By.id("inactiveAccountForm:emailField:updateEmail"));
+        clickElement(By.id("inactiveAccountForm:email:input:updateEmail"));
         return new HomePage(getDriver());
     }
 }
